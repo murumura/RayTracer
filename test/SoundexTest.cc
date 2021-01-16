2b3afdebc70cc4d5d2cd497a5c7928629378448e
steven <porn52hub78@gmail.com>
2021-01-16 23:30:52 +0800

    write some test code

--- test/SoundexTest.cc
+++ test/SoundexTest.cc
@@ -1,7 +1,49 @@
 #include "gmock/gmock.h"    
-class Soundex {
+#include <string>
+using testing::Eq;
+class Soundex
+{
+public:
+   static const size_t MaxCodeLength{4};
+   // ...
+
+   std::string encode(const std::string& word) const {
+      return zeroPad(head(word) + encodedDigits(word));
+   }
+
+private:
+   std::string head(const std::string& word) const {
+      return word.substr(0, 1);
+   }
+
+   std::string encodedDigits(const std::string& word) const {
+      if (word.length() > 1) return encodedDigits();
+      return "";
+   }
+   std::string encodedDigits() const {
+      return "1";
+   }
+
+   std::string zeroPad(const std::string& word) const {
+      auto zerosNeeded = MaxCodeLength - word.length();
+      return word + std::string(zerosNeeded, '0');
+   }
 };
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { 
-   Soundex soundex;   
+class SoundexEncoding: public testing::Test {
+public:
+   Soundex soundex;
+};
+
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+   ASSERT_THAT(soundex.encode("A"), Eq("A000")); 
+}
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+   ASSERT_THAT(soundex.encode("I"), Eq("I000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+   ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
 }
 
